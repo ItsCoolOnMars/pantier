ff4317dbb9604a309ad00f2ff089c99250b5df25
Maxim Musikhin <133215208+MaximOlsights@users.noreply.github.com>
2024-06-26 16:10:02 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    function authorizeMyAnimeList() {
+        const clientId = ''; 
+        const redirectUri = encodeURIComponent(''); 
+        const responseType = 'token';
+        const state = ''; 
+        const scope = 'write:users';
+
+        const authorizationUrl = `https://myanimelist.net/v1/oauth2/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
+
+        window.location.href = authorizationUrl;
+    }
+</script>
+
+<button on:click={authorizeMyAnimeList}>Authorize MyAnimeList</button>
